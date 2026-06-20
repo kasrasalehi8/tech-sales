c2d97b44b87a725b2fdea4842b20b510b00c4087
Arya Baladi <ariabaladi.ab@gmail.com>
2026-06-20 03:31:55 +0330

    Add DashboardService

---
+++ TechSales.Application/DTO/DashboardStatsDto.cs
@@ -0,0 +1,19 @@
+﻿namespace TechSales.Application.DTO
+{
+    public class DashboardStatsDto
+    {
+        public decimal TotalRevenue { get; set; }
+
+        public int TotalCustomers { get; set; }
+
+        public int TotalProducts { get; set; }
+
+        public int TotalInvoices { get; set; }
+
+        public decimal OutstandingBalance { get; set; }
+
+        public int PaidInvoices { get; set; }
+
+        public int UnpaidInvoices { get; set; }
+    }
+}
---
+++ TechSales.Application/Services/DashboardService.cs
@@ -0,0 +1,36 @@
+﻿using TechSales.Application.DTO;
+using TechSales.Domain.Enums;
+using TechSales.Infrastructure.Persistence;
+
+namespace TechSales.Application.Services
+{
+    public class DashboardService
+    {
+        private readonly TechSalesDbContext _db;
+
+        public DashboardService(TechSalesDbContext db)
+        {
+            _db = db;
+        }
+
+        public DashboardStatsDto GetStats()
+        {
+            return new DashboardStatsDto
+            {
+                TotalRevenue = _db.Payments.Sum(p => p.Amount),
+
+                TotalCustomers = _db.Customers.Count(),
+
+                TotalProducts = _db.Products.Count(p => p.IsActive),
+
+                TotalInvoices = _db.Invoices.Count(),
+
+                OutstandingBalance = _db.Invoices.Sum(i => i.TotalAmount - i.PaidAmount),
+
+                PaidInvoices = _db.Invoices.Count(i => i.Status == (int)InvoiceStatus.Paid),
+
+                UnpaidInvoices = _db.Invoices.Count(i => i.Status != (int)InvoiceStatus.Paid)
+            };
+        }
+    }
+}
