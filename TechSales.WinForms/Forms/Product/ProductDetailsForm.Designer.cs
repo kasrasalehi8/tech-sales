7dbacf0783fb17b22b1e5972b8cf3b94060e5dcb
Arya Baladi <ariabaladi.ab@gmail.com>
2026-06-29 22:46:33 +0330

    Fix label name in Product Details page

--- TechSales.WinForms/Forms/Product/ProductDetailsForm.Designer.cs
+++ TechSales.WinForms/Forms/Product/ProductDetailsForm.Designer.cs
@@ -54,9 +54,9 @@
             lblNameTitle.BackColor = Color.Transparent;
             lblNameTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblNameTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblNameTitle.Location = new Point(12, 10);
+            lblNameTitle.Location = new Point(14, 13);
             lblNameTitle.Name = "lblNameTitle";
-            lblNameTitle.Size = new Size(57, 21);
+            lblNameTitle.Size = new Size(71, 28);
             lblNameTitle.TabIndex = 0;
             lblNameTitle.Text = "Name:";
             // 
@@ -66,9 +66,9 @@
             lblCategoryTitle.BackColor = Color.Transparent;
             lblCategoryTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblCategoryTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblCategoryTitle.Location = new Point(12, 40);
+            lblCategoryTitle.Location = new Point(14, 53);
             lblCategoryTitle.Name = "lblCategoryTitle";
-            lblCategoryTitle.Size = new Size(82, 21);
+            lblCategoryTitle.Size = new Size(99, 28);
             lblCategoryTitle.TabIndex = 1;
             lblCategoryTitle.Text = "Category:";
             // 
@@ -78,9 +78,9 @@
             lblSupplierTitle.BackColor = Color.Transparent;
             lblSupplierTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblSupplierTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblSupplierTitle.Location = new Point(12, 70);
+            lblSupplierTitle.Location = new Point(14, 93);
             lblSupplierTitle.Name = "lblSupplierTitle";
-            lblSupplierTitle.Size = new Size(76, 21);
+            lblSupplierTitle.Size = new Size(93, 28);
             lblSupplierTitle.TabIndex = 2;
             lblSupplierTitle.Text = "Supplier:";
             // 
@@ -90,9 +90,9 @@
             lblPriceTitle.BackColor = Color.Transparent;
             lblPriceTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblPriceTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblPriceTitle.Location = new Point(12, 100);
+            lblPriceTitle.Location = new Point(14, 133);
             lblPriceTitle.Name = "lblPriceTitle";
-            lblPriceTitle.Size = new Size(50, 21);
+            lblPriceTitle.Size = new Size(61, 28);
             lblPriceTitle.TabIndex = 3;
             lblPriceTitle.Text = "Price:";
             // 
@@ -102,9 +102,9 @@
             lblStockTitle.BackColor = Color.Transparent;
             lblStockTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblStockTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblStockTitle.Location = new Point(12, 130);
+            lblStockTitle.Location = new Point(14, 173);
             lblStockTitle.Name = "lblStockTitle";
-            lblStockTitle.Size = new Size(56, 21);
+            lblStockTitle.Size = new Size(67, 28);
             lblStockTitle.TabIndex = 4;
             lblStockTitle.Text = "Stock:";
             // 
@@ -114,9 +114,9 @@
             lblStatusTitle.BackColor = Color.Transparent;
             lblStatusTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblStatusTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblStatusTitle.Location = new Point(12, 160);
+            lblStatusTitle.Location = new Point(14, 213);
             lblStatusTitle.Name = "lblStatusTitle";
-            lblStatusTitle.Size = new Size(59, 21);
+            lblStatusTitle.Size = new Size(72, 28);
             lblStatusTitle.TabIndex = 5;
             lblStatusTitle.Text = "Status:";
             // 
@@ -126,9 +126,9 @@
             lblNameValue.BackColor = Color.Transparent;
             lblNameValue.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblNameValue.ForeColor = Color.Black;
-            lblNameValue.Location = new Point(72, 10);
+            lblNameValue.Location = new Point(82, 13);
             lblNameValue.Name = "lblNameValue";
-            lblNameValue.Size = new Size(40, 21);
+            lblNameValue.Size = new Size(48, 28);
             lblNameValue.TabIndex = 6;
             lblNameValue.Text = "N/A";
             // 
@@ -138,9 +138,9 @@
             lblCategoryValue.BackColor = Color.Transparent;
             lblCategoryValue.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblCategoryValue.ForeColor = Color.Black;
-            lblCategoryValue.Location = new Point(99, 40);
+            lblCategoryValue.Location = new Point(113, 53);
             lblCategoryValue.Name = "lblCategoryValue";
-            lblCategoryValue.Size = new Size(40, 21);
+            lblCategoryValue.Size = new Size(48, 28);
             lblCategoryValue.TabIndex = 7;
             lblCategoryValue.Text = "N/A";
             // 
@@ -150,9 +150,9 @@
             lblSupplierValue.BackColor = Color.Transparent;
             lblSupplierValue.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblSupplierValue.ForeColor = Color.Black;
-            lblSupplierValue.Location = new Point(94, 70);
+            lblSupplierValue.Location = new Point(107, 93);
             lblSupplierValue.Name = "lblSupplierValue";
-            lblSupplierValue.Size = new Size(40, 21);
+            lblSupplierValue.Size = new Size(48, 28);
             lblSupplierValue.TabIndex = 8;
             lblSupplierValue.Text = "N/A";
             // 
@@ -162,9 +162,9 @@
             lblPriceValue.BackColor = Color.Transparent;
             lblPriceValue.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblPriceValue.ForeColor = Color.Black;
-            lblPriceValue.Location = new Point(68, 100);
+            lblPriceValue.Location = new Point(78, 133);
             lblPriceValue.Name = "lblPriceValue";
-            lblPriceValue.Size = new Size(40, 21);
+            lblPriceValue.Size = new Size(48, 28);
             lblPriceValue.TabIndex = 9;
             lblPriceValue.Text = "N/A";
             // 
@@ -174,9 +174,9 @@
             lblStockValue.BackColor = Color.Transparent;
             lblStockValue.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblStockValue.ForeColor = Color.Black;
-            lblStockValue.Location = new Point(74, 130);
+            lblStockValue.Location = new Point(85, 173);
             lblStockValue.Name = "lblStockValue";
-            lblStockValue.Size = new Size(40, 21);
+            lblStockValue.Size = new Size(48, 28);
             lblStockValue.TabIndex = 10;
             lblStockValue.Text = "N/A";
             // 
@@ -186,9 +186,9 @@
             lblStatusValue.BackColor = Color.Transparent;
             lblStatusValue.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblStatusValue.ForeColor = Color.Black;
-            lblStatusValue.Location = new Point(77, 160);
+            lblStatusValue.Location = new Point(88, 213);
             lblStatusValue.Name = "lblStatusValue";
-            lblStatusValue.Size = new Size(40, 21);
+            lblStatusValue.Size = new Size(48, 28);
             lblStatusValue.TabIndex = 11;
             lblStatusValue.Text = "N/A";
             // 
@@ -200,9 +200,10 @@
             btnDelete.FlatStyle = FlatStyle.Flat;
             btnDelete.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnDelete.ForeColor = Color.White;
-            btnDelete.Location = new Point(125, 246);
+            btnDelete.Location = new Point(143, 328);
+            btnDelete.Margin = new Padding(3, 4, 3, 4);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(80, 35);
+            btnDelete.Size = new Size(91, 47);
             btnDelete.TabIndex = 14;
             btnDelete.Text = "Delete";
             btnDelete.UseVisualStyleBackColor = false;
@@ -210,9 +211,10 @@
             // 
             // numAddStock
             // 
-            numAddStock.Location = new Point(77, 190);
+            numAddStock.Location = new Point(74, 253);
+            numAddStock.Margin = new Padding(3, 4, 3, 4);
             numAddStock.Name = "numAddStock";
-            numAddStock.Size = new Size(120, 23);
+            numAddStock.Size = new Size(137, 27);
             numAddStock.TabIndex = 15;
             // 
             // lblAddStockTitle
@@ -221,11 +223,11 @@
             lblAddStockTitle.BackColor = Color.Transparent;
             lblAddStockTitle.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblAddStockTitle.ForeColor = Color.FromArgb(64, 64, 64);
-            lblAddStockTitle.Location = new Point(12, 190);
+            lblAddStockTitle.Location = new Point(14, 253);
             lblAddStockTitle.Name = "lblAddStockTitle";
-            lblAddStockTitle.Size = new Size(59, 21);
+            lblAddStockTitle.Size = new Size(54, 28);
             lblAddStockTitle.TabIndex = 16;
-            lblAddStockTitle.Text = "Status:";
+            lblAddStockTitle.Text = "Add:";
             // 
             // btnSave
             // 
@@ -235,9 +237,10 @@
             btnSave.FlatStyle = FlatStyle.Flat;
             btnSave.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnSave.ForeColor = Color.White;
-            btnSave.Location = new Point(224, 190);
+            btnSave.Location = new Point(256, 252);
+            btnSave.Margin = new Padding(3, 4, 3, 4);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(73, 23);
+            btnSave.Size = new Size(83, 31);
             btnSave.TabIndex = 63;
             btnSave.Text = "+ Add";
             btnSave.UseVisualStyleBackColor = false;
@@ -251,9 +254,10 @@
             btnCancel.FlatStyle = FlatStyle.Flat;
             btnCancel.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnCancel.ForeColor = Color.FromArgb(3, 38, 53);
-            btnCancel.Location = new Point(217, 246);
+            btnCancel.Location = new Point(248, 328);
+            btnCancel.Margin = new Padding(3, 4, 3, 4);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(80, 35);
+            btnCancel.Size = new Size(91, 47);
             btnCancel.TabIndex = 64;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = false;
@@ -261,10 +265,10 @@
             // 
             // ProductDetailsForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(309, 293);
+            ClientSize = new Size(353, 391);
             Controls.Add(btnCancel);
             Controls.Add(btnSave);
             Controls.Add(lblAddStockTitle);
@@ -283,6 +287,7 @@
             Controls.Add(lblCategoryTitle);
             Controls.Add(lblNameTitle);
             FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ProductDetailsForm";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Product Details";
